d7676870dcbf5f3dca0641085794444a44d32a4e
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-10-20 10:08:16 -0400

    adding

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+/**
+ * Title: user.js
+ * Author: Diandra McKenzie
+ * Date: 20 October 2020
+ * Description: User Model file
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const securityQuestion = require('./security-question');
+
+/**
+ * user schema, sprint 1
+ */
+
+let userSchema = new Schema ({
+   userName: {type: String, required:true, unique: true, dropDups: true},
+   password: {type: String, required: true},
+   firstName: {type: String},
+   lastName: {type: String},
+   phoneNumber: {type: String},
+   address: {type: String},
+   email: {type: String},
+   isDisabled: {type: Boolean, default: false},
+   role: {type: String, default: 'standard'},
+   securityQuestions: [securityQuestions],
+   date_created: {type: Date, default: new Date()},
+   date_modified: {type: Date}
+ }, { collection: 'user'});
+
+module.exports = mongoose.model('User', userSchema);
