ce6c379b90e91bc5e5d8f404b8f032694e82279f
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-10-22 10:23:36 -0400

    update

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -5,3 +5,14 @@
  ; Modified By: Diandra McKenzie
  ; Description: User Interface
  ===========================================*/
+
+ export interface User {
+   _id: string;
+   userName: string;
+   password: string;
+   firstName: string;
+   lastName: string;
+   phoneNumber: string;
+   address: string;
+   email: string;
+ }
