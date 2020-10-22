a5300730d9282c85903d57006399dfd216ded702
Jeff Lintel <jefflintel@gmail.com>
2020-10-21 21:01:35 -0500

    updated security question model and api

--- server/routes/security-questions-api.js
+++ server/routes/security-questions-api.js
@@ -14,6 +14,7 @@ const securityQuestion = require('../models/security-question');
 const BaseResponse = require('../services/base-response');
 const ErrorResponse = require('../services/error-response');
 const User = require('../models/user');
+const securityQuestionSchema = require('../models/security-question');
 
 
 const router= express.Router();
@@ -58,6 +59,49 @@ router.get('', async(req, res) => {
  //find all security questions by id
 
  //create security question
+
+ router.post(':/_id', async(req, res) => {
+   try {
+     securityQuestion.findOne({'_id': req.params._id}, function(err, question) {
+       if (err) {
+         console.log(err);
+
+         const createSecurityQuestionMongoDbErrorResponse = new ErrorResponse('500', 'Internal server error', err);
+
+         res.status(500).send(createSecurityQuestionMongoDbErrorResponse.toObject());
+       } else {
+
+        const question = {
+          text: req.body.text
+        }
+
+        securityQuestion.push(question);
+        securityQuestion.save(function(err, updatedQuestion) {
+          if (err) {
+            console.log(err);
+
+            const createSecurityQuestionOnSaveMongoDbErrorResponse = new ErrorResponse ('500', 'Internal server error', err);
+
+            res.status(500).send(createSecurityQuestionOnSaveMongoDbErrorResponse.toObject());
+          } else {
+
+            console.log(updatedQuestion);
+
+            const createSecurityQuestionOnSaveSuccessResponse = new BaseResponse('200', 'Entry successful', updatedQuestion);
+
+            res.json(createSecurityQuestionOnSaveSuccessResponse.toObject());
+          }
+        })
+       }
+     })
+   } catch (e) {
+     console.log(e);
+
+     const createQuestionCatchErrorResponse = new ErrorResponse('500', 'Internal server error', e.message);
+
+     res.status(500).send(createQuestionCatchErrorResponse.toObject());
+   }
+ })
 
 
 /**
