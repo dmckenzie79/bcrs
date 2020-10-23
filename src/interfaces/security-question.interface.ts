d781a46605ddc9119af2017758afaa17aac570ff
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-10-22 21:29:25 -0400

    update

--- src/interfaces/security-question.interface.ts
+++
@@ -1,7 +0,0 @@
-/* ============================================
- ; Title:  security-question.interface.ts
- ; Author: Professor Krasso
- ; Date:   22 October 2020
- ; Modified By: Diandra McKenzie
- ; Description: Security Question Interface
- ===========================================*/
