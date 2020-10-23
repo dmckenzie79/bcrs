a05b27ef6e6204b4a99edd1b6cd1cf0664875a32
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-10-22 21:28:55 -0400

    adding

---
+++ src/app/shared/interfaces/security-question.interface.ts
@@ -0,0 +1,12 @@
+/* ============================================
+ ; Title:  security-question.interface.ts
+ ; Author: Professor Krasso
+ ; Date:   22 October 2020
+ ; Modified By: Diandra McKenzie
+ ; Description: Security Question Interface
+ ===========================================*/
+
+export interface SecurityQuestion {
+  _id: string;
+  text: string;
+}
