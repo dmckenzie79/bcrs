022f2a9ffdce17b63142ee66db9513f378061a85
Jeff Lintel <jefflintel@gmail.com>
2020-10-24 22:14:44 -0500

    updated user details component

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from '../../user.service';
+import { User } from './../../../user.interface';
 
 @Component({
   selector: 'app-user-details',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  userId: string;
+  form: FormGroup;
+  roles: any;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private fb: FormBuilder, private router: Router, private userService: UserService) {
+    this.userId = this.route.snapshot.paramMap.get('userId');
+
+    this.userService.findUserById(this.userId).subscribe(res => {
+      this.user = res['data'];
+    }, err => {
+      console.log(err)
+    }, () => {
+      this.form.controls.firstName.setValue(this.user.firstName);
+      this.form.controls.lastName.setValue(this.user.lastName);
+      this.form.controls.phoneNumber.setValue(this.user.phoneNumber);
+      this.form.controls.address.setValue(this.user.phoneNumber);
+      this.form.controls.email.setValue(this.user.email);
+    })
+  }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      firstName: [null, Validators.compose([Validators.required])],
+      lastName: [null, Validators.compose([Validators.required])],
+      phoneNumber: [null, Validators.compose([Validators.required])],
+      address: [null, Validators.compose([Validators.required])],
+      email: [null, Validators.compose([Validators.required, Validators.email])]
+    })
+  }
+
+  saveUser() {
+    const updatedUser = {} as User;
+    updatedUser.firstName = this.form.controls.firstName.value;
+    updatedUser.lastName = this.form.controls.lastName.value;
+    updatedUser.phoneNumber = this.form.controls.phoneNumber.value;
+    updatedUser.address = this.form.controls.address.value;
+    updatedUser.email = this.form.controls.email.value;
+
+    this.userService.updateUser(this.userId, updatedUser).subscribe(res => {
+      this.router.navigate(['/users'])
+    })
+  }
+
+  cancel() {
+    this.router.navigate(['/users'])
   }
 
 }
