ddf635ff5fa526995984c152931c48ef02db1f42
Jeff Lintel <jefflintel@gmail.com>
2020-10-25 23:55:59 -0500

    attempt at drag and drop columns

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -5,59 +5,64 @@
  ; Description: user list component
  ===========================================*/
 
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { HttpClient } from '@angular/common/http';
-import { UserService } from './../../user.service';
-import { User } from './../../../user.interface';
-import { DeleteRecordDialogComponent } from 'src/app/shared/delete-record-dialog/delete-record-dialog.component';
+ import { Component, OnInit } from '@angular/core';
+ import { MatDialog } from '@angular/material/dialog';
+ import { HttpClient } from '@angular/common/http';
+ import { UserService } from './../../user.service';
+ import { User } from './../../../user.interface';
+ import { DeleteRecordDialogComponent } from 'src/app/shared/delete-record-dialog/delete-record-dialog.component';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
 
 
-@Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
-})
-export class UserListComponent implements OnInit {
+ @Component({
+   selector: 'app-user-list',
+   templateUrl: './user-list.component.html',
+   styleUrls: ['./user-list.component.css']
+ })
+ export class UserListComponent implements OnInit {
 
-  users: User[];
+   users: User[];
 
+   //columns of user data to be displayed
+   displayedColumns: string[] = ['userName', 'firstName', 'lastName', 'phoneNumber', 'address', 'email', 'functions'];
 
-  //columns of user data to be displayed
-  displayedColumns = ['userName', 'firstName', 'lastName', 'phoneNumber', 'address', 'email', 'functions'];
+    //reorder columns with drag/drop
+    drop(event: CdkDragDrop<string[]> ) {
+      moveItemInArray(this.displayedColumns, event.previousIndex, event.currentIndex);
+    }
 
-  constructor(private http: HttpClient, private dialog: MatDialog, private userService: UserService) {
+   constructor(private http: HttpClient, private dialog: MatDialog, private userService: UserService) {
 
-    this.userService.findAllUsers().subscribe(res => {
-      this.users = res['data'];
-      console.log(this.users);
-    }, err => {
-      console.log(err)
-    });
-  }
+     this.userService.findAllUsers().subscribe(res => {
+       this.users = res['data'];
+       console.log(this.users);
+     }, err => {
+       console.log(err)
+     });
+   }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+   }
 
-  delete(userId, recordId) {
-    const dialogRef = this.dialog.open(DeleteRecordDialogComponent, {
-      data: {
-        recordId,
-        dialogHeader: 'Delete Record?',
-        dialogBody: `Are you sure you want to delete user ${recordId}`
-      },
-      disableClose: true,
-      width: '720px'
-    });
+   delete(userId, recordId) {
+     const dialogRef = this.dialog.open(DeleteRecordDialogComponent, {
+       data: {
+         recordId,
+         dialogHeader: 'Delete Record?',
+         dialogBody: `Are you sure you want to delete user ${recordId}`
+       },
+       disableClose: true,
+       width: '720px'
+     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result === 'confirm') {
-        this.userService.deleteUser(userId).subscribe(res => {
-          console.log('User delete');
-          this.users = this.users.filter(u => u._id !== userId);
-        })
-      }
-    });
-  }
-}
+     dialogRef.afterClosed().subscribe(result => {
+       if (result === 'confirm') {
+         this.userService.deleteUser(userId).subscribe(res => {
+           console.log('User delete');
+           this.users = this.users.filter(u => u._id !== userId);
+         })
+       }
+     });
+   }
+ }
