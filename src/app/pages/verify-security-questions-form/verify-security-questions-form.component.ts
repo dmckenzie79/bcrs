4a8c6d3f6b8b6dc9031adea11b7f9c3e4ad41dab
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-10-29 19:40:57 -0400

    adding

---
+++ src/app/pages/verify-security-questions-form/verify-security-questions-form.component.ts
@@ -0,0 +1,23 @@
+/* ============================================
+ ; Title:  verify-security-questions-form.component.ts
+ ; Author: Professor Krasso
+ ; Date:   29 October 2020
+ ; Modified By: Diandra McKenzie, Jeff Lintel, Zach Dahir
+ ; Description: Verify Security Questions component
+ ===========================================*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-security-questions-form',
+  templateUrl: './verify-security-questions-form.component.html',
+  styleUrls: ['./verify-security-questions-form.component.css']
+})
+export class VerifySecurityQuestionsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/verify-username-form/verify-username-form.component.ts
@@ -0,0 +1,23 @@
+/* ============================================
+ ; Title:  verify-username-form.component.ts
+ ; Author: Professor Krasso
+ ; Date:   29 October 2020
+ ; Modified By: Diandra McKenzie, Jeff Lintel, Zach Dahir
+ ; Description: Verify User Name component
+ ===========================================*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-username-form',
+  templateUrl: './verify-username-form.component.html',
+  styleUrls: ['./verify-username-form.component.css']
+})
+export class VerifyUsernameFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
