b208b001130dd471db32a2b0cf035613558f3ac2
Jeff Lintel <jefflintel@gmail.com>
2020-11-05 20:36:23 -0600

    added find purchases by service and server message updates

--- server/routes/invoice-api.js
+++ server/routes/invoice-api.js
@@ -11,8 +11,15 @@ const Invoice = require('../models/invoice');
 const LineItem = require('../schemas/line-item');
 const BaseResponse = require('../services/base-response');
 const ErrorResponse = require('../services/error-response');
+const invoice = require('../models/invoice');
 
 const router= express.Router();
+
+//constants to pass into server responses
+const serverError = "Internal server error";
+const querySuccess = "Query successful - ";
+const createInvoice = "create invoice";
+const findPurchasesByService = "find purchases by service";
 
 //create invoice api
 router.post('/:userName', async(req, res) => {
@@ -36,19 +43,66 @@ router.post('/:userName', async(req, res) => {
     {
       if (err) {
         console.log(err);
-        const createInvoiceMongodbErrorResponse = new ErrorResponse('500', 'Internal Server Error', err);
+        const createInvoiceMongodbErrorResponse = new ErrorResponse('500', serverError, err); //updated to use message constants
         res.status(500).send(createInvoiceMongodbErrorResponse.toObject());
       }
       else {
         console.log(invoice);
-        const createInvoiceResponse = new BaseResponse('200', 'Invoice created', invoice);
+        const createInvoiceResponse = new BaseResponse('200', `${querySuccess} ${createInvoice}`, invoice); //updated to use message constants
         res.json(createInvoiceResponse.toObject());
       }
     })
   }
   catch (e) {
     console.log(e);
-    const createInvoiceCatchErrorResponse = new ErrorResponse('500', 'Internal Server Error', e.message);
+    const createInvoiceCatchErrorResponse = new ErrorResponse('500', serverError, e.message); //updated to use message constants
     res.status(500).send(createInvoiceCatchErrorResponse.toObject());
   }
 });
+
+//find purchases by service (for data visualization)
+router.get('/purchases-graph', async(req, res) => {
+  try{
+    Invoice.aggregate([
+      {
+        $unwind: '$lineItems'
+      },
+      {
+        $group:
+        {
+          '_id':
+          {
+            'title': '$lineItems.title',
+            'price': '$lineItems.price'
+          },
+          'count':
+          {
+            $sum: 1
+          }
+        }
+      },
+      {
+        $sort:
+        {
+          '_id.title': 1
+        }
+      }
+    ], function(err, purchaseGraph) {
+      if(err) {
+        console.log(err);
+        const findPurchasesByServiceGraphMongoDbErrorResponse = new ErrorResponse('500', serverError, err);
+        res.status(500).send(findPurchasesByServiceGraphMongoDbErrorResponse.toObject());
+      } else {
+        console.log(purchaseGraph);
+        const findPurchasesByServiceGraphResponse = new BaseResponse('200', `${querySuccess} ${findPurchasesByService}`, purchaseGraph);
+        res.json(findPurchasesByServiceGraphResponse.toObject());
+      }
+    })
+  } catch(e) {
+    console.log(e);
+    const findPurchasesByServiceCatchErrorResponse = new ErrorResponse('500', serverError, e.message);
+    res.status(500).send(findPurchasesByServiceCatchErrorResponse.toObject());
+  }
+});
+
+module.exports = router;
